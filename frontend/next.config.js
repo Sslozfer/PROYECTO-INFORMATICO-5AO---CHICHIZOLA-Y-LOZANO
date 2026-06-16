0061a1bed20600c7afcb573f58ec882a0476bce5
Sslozfer <sslozfer@gmail.com>
2026-06-16 11:41:11 -0300

    Fix Vercel deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-      ignoreDuringBuilds: true,
-    },
+    ignoreDuringBuilds: true,
+  },
   typescript: {
-      ignoreBuildErrors: true,
-    },
-  output: 'standalone',
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
   },
 };
 
